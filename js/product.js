158a872f816ac9c9817419201e95db85c2fda1d5
Abdulholiq13 <abdulholiq571@gmail.com>
2024-07-27 22:59:54 +0500

    add detail page

--- js/product.js
+++ js/product.js
@@ -40,6 +40,31 @@ function createContent(data) {
 		</p>
 
 		<hr class="content__line" />
+
+        <div class="content__color-wrapper">
+            <p class="content__color-text">Colours:</p>
+			<img class="content__color" src="../images/svg/blue-round.svg" alt="" width="20" height="20" />
+			<img class="content__color" src="../images/svg/red-round.svg" alt="" width="20" height="20" />
+		</div>
+		<div class="content__sizes">
+			<p class="content__size-text">Size:</p>
+			<span class="content__size">XS</span>
+			<span class="content__size">S</span>
+			<span class="content__size">M</span>
+			<span class="content__size">L</span>
+			<span class="content__size">XL</span>
+		</div>
+		<div class="content__count-wrapper">
+		    <div class="content__count-inner">
+			    <button class="content__count-inner-btn">-</button>
+					<span class="content__count-text">0</span>
+				<button class="content__count-inner-btn plus">+</button>
+			</div>
+			    <button class="btn-secondary">Buy Now</button>
+				<button class="content__count-heart">
+				    <img src="../images/svg/heart-small-icon.svg" alt="" width="32" height="32"/>
+				</button>
+			</div>
 	</div>
     `;
 }
